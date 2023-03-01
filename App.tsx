01adf566bd69c7862f1b3eccba216d7e7bf47472
Lucas Cortinhas <lucascortinhas.lc@gmail.com>
2023-03-01 17:53:07 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import React, {type PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView>
+      <Text>hello world</Text>
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
