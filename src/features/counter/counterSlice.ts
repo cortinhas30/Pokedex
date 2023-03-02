e9008f44ba28368ecc9a5b7859eeb34ff4afb5d6
Lucas Cortinhas <lucascortinhas.lc@gmail.com>
2023-03-02 17:26:24 -0300

    counter

--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -1,1 +1,39 @@
+/* eslint-disable eol-last*/
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+/* eslint-disable prettier/prettier */
+
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface CounterState{
+    value: number;
+}
+
+const initialState: CounterState = {
+    value: 0,
+}
+
+const counterSlice = createSlice({
+    name: "counter",
+    initialState,
+    reducers: {
+        increment: (state) => {
+            state.value ++;
+        }
+        decrement: (state) => {
+            state.value --;
+        },
+        incrementByAmount: (state, action: PayloadAction<number>) => {
+            state.value += action.payload;
+        },
+        decrementByAmount: (state, action: PayloadAction<number>) => {
+            state.value -= action.payload;
+        },
+        reset: (state) => {
+            state.value = initialState.value;
+        }
+    }
+});
+
+export const {increment, decrement, incrementByAmount, decrementByAmount, reset} = counterSlice.actions;
+export default counterSlice.reducer;
