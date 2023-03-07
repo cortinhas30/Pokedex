adceaa7979302bf466c12101b9cfa2f5a5ba056b
Lucas Cortinhas <lucascortinhas.lc@gmail.com>
2023-03-07 18:19:36 -0300

    Buttons

--- src/components/ButtonRedux.tsx
+++ src/components/ButtonRedux.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { TouchableOpacity, Text, View } from 'react-native';
+import { useAppDispatch } from '../app/hooks';
+import { increment, decrement, incrementByAmount } from '../features/counter/counterSlice';
 
 const ButtonRedux = () =>{
 
+    const dispatch = useAppDispatch();
+
     return(
+        <>
         <TouchableOpacity
+            onPress={() => dispatch(increment())}
             style={{
                 backgroundColor: 'black',
                 padding: 10,
@@ -14,6 +20,29 @@ const ButtonRedux = () =>{
         >
             <Text style={{ color: "white", textAlign: "center", fontWeight: "bold" }}>Increment</Text>
         </TouchableOpacity>
+        <TouchableOpacity
+            onPress={() => dispatch(decrement())}
+            style={{
+                backgroundColor: '#29938d',
+                padding: 10,
+                borderRadius: 5,
+                margin: 10,
+            }}
+        >
+            <Text style={{ color: "white", textAlign: "center", fontWeight: "bold" }}>Decrement</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+            onPress={() => dispatch(incrementByAmount(10))}
+            style={{
+                backgroundColor: '#5f32b8',
+                padding: 10,
+                borderRadius: 5,
+                margin: 10,
+            }}
+        >
+            <Text style={{ color: "white", textAlign: "center", fontWeight: "bold" }}>Ad 10</Text>
+        </TouchableOpacity>
+        </>
     );
 };
 
